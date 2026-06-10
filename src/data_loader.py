b91c735c7a732b59b5afd3d5cdae6f35f9c45320
axelw <axel.walraet-triolet.1@ens.etsmtl.ca>
2026-06-10 16:13:05 -0400

    Modif de commentaires

--- src/data_loader.py
+++ src/data_loader.py
@@ -13,13 +13,11 @@ class F1DataLoader:
         fastf1.Cache.enable_cache(cache_dir)
 
     def load_session_data(self, year, gp, event_type):
-        """Loads a specific F1 session (e.g., year=2025, gp='Monaco', event_type='Q')"""
         session = fastf1.get_session(year, gp, event_type)
         session.load()
         return session
 
     def get_driver_telemetry(self, session, driver_code):
-        """Returns the telemetry dataframe for a specific driver"""
         lap = session.laps.pick_driver(driver_code).pick_fastest()
         telemetry = lap.get_telemetry()
         return telemetry
@@ -27,7 +25,7 @@ class F1DataLoader:
     def get_event_laps_count(self, year, gp_name, event_type='R'):
         """
         Récupère le nombre total de tours pour un GP donné
-        sans charger toute la télémétrie lourde.
+        sans charger toute la télémétrie.
         """
         import fastf1
         try:
@@ -64,7 +62,7 @@ class F1DataLoader:
 
     def load_multi_season_coefficients(self):
         """
-        Charge instantanément les coefficients pluri-annuels isotoniques
+        Charge  les coefficients pluri-annuels
         stockés à la racine du projet.
         """
         import json
